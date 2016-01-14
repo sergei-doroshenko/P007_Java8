0f7e3ec747356b40395c665085b21702a2be0def
Sergei <6823298@gmail.com>
2016-01-14 22:19:15 +0300

    Research functional interfaces.

--- src/main/java/org/sergei/j8/Resource.java
+++ src/main/java/org/sergei/j8/Resource.java
@@ -1,11 +1,13 @@
 package org.sergei.j8;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Created by sergei on 1/14/16.
  */
 public class Resource {
+
     private Resource() { System.out.println("Created by use..."); }
 
     public void op1() {
@@ -28,4 +30,37 @@ public class Resource {
             resource.close();
         }
     }
+
+    public String op3() {
+        return "Some operation 3...";
+    }
+
+    public String op4() {
+        return "Some operation 4...";
+    }
+
+    public static String cat(Function<Resource, String> block) {
+        Resource resource = new Resource();
+        try {
+            return block.apply(resource);
+        } finally {
+            resource.close();
+        }
+    }
+
+    public Function<Resource, String> fun1 = resource -> "Some function 1...";
+
+    public Function<String, Integer> fun2 = str -> str.length();
+
+
+    public static String compute(Function<Resource, Integer> block) {
+        Resource resource = new Resource();
+        try {
+            return block.apply(resource).toString();
+        } finally {
+            resource.close();
+        }
+    }
+
+
 }
--- src/main/java/org/sergei/j8/Sample06.java
+++ src/main/java/org/sergei/j8/Sample06.java
@@ -1,6 +1,7 @@
 package org.sergei.j8;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * Created by sergei on 1/14/16.
@@ -25,5 +26,20 @@ public class Sample06 {
             resource.op1();
             resource.op2();
         });
+
+        /*String result = Resource.compute(new Function<Resource, String>() {
+            @Override
+            public String apply(Resource resource) {
+                return resource.op3();
+            }
+        });*/
+        /*String result = Resource.compute(Resource::op3);*/
+        String result1 = Resource.cat(resource -> resource.op3() + resource.op4());
+        System.out.println(result1);
+
+        String result2 = Resource.compute(resource ->
+                        resource.fun1.andThen(resource.fun2).apply(resource)
+        );
+        System.out.println(result2);
     }
 }
