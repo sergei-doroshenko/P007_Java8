f695d868e836b2e1d84b22aa277f728ac282a002
sergei-doroshenko <Astro15pa$$>
2016-02-24 15:33:09 +0300

    Added dates sort.

--- src/main/java/org/sergei/j8/Sample02.java
+++ src/main/java/org/sergei/j8/Sample02.java
@@ -1,7 +1,7 @@
 package org.sergei.j8;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.reflect.Method;
+import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -63,5 +63,32 @@ public class Sample02 {
                         sqrtOfFirst100Primes.get(sqrtOfFirst100Primes.size() - 1))
         );
 
+
+        testDates();
+    }
+
+    public static void testDates () {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
+        calendar.set(2016, Calendar.JANUARY, 5, 12, 30, 0);
+
+        Date d0 = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, -20);
+        Date d1 = calendar.getTime();
+        calendar.add(Calendar.DAY_OF_YEAR, 63);
+        Date d2 = calendar.getTime();
+
+
+        List<Date> dates = Arrays.asList(new Date[]{
+                d0, d1, d2
+        });
+
+//        Optional<Date> min = dates.stream().min((o1, o2) -> o1.compareTo(o2));
+        Optional<Date> min = dates.stream().min(Comparator.naturalOrder());
+        System.out.println(min.get());
+
+        Optional<Date> max = dates.stream().max((o1, o2) -> o1.compareTo(o2));
+        System.out.println(max.get());
+//        dates.stream().sorted().forEach(d -> System.out.println(d));
     }
 }
